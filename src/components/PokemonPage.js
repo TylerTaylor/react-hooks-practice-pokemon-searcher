93ed9e0ced9a88c4b95365d9565ba334af2d8b58
TylerTaylor <vikingtyty@gmail.com>
2022-09-27 13:07:17 -0500

    Finished

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  // set up state functionality (pokemon, search term)
+  const [pokemon, setPokemon] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+
+  // fetch our data when this component loads
+  useEffect(() => {
+    fetch('http://localhost:3001/pokemon')
+      .then(res => res.json())
+      .then(data => setPokemon(data))
+      // .then(setPokemon) <- alternative approach to the line above
+  }, [])
+
+  // need a function to add single pokemon to our state
+  const handleAddPokemon = (newPokemon) => {
+    setPokemon([newPokemon, ...pokemon]) // add to front of list
+    // setPokemon([...pokemon, newPokemon]) // add to back of list
+  }
+
+  // filter our pokemon data
+  const pokemonToDisplay = pokemon.filter(poke => 
+    poke.name.toLowerCase().includes(searchTerm.toLowerCase())  
+  )
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm onAddPokemon={handleAddPokemon} />
       <br />
-      <Search />
+      <Search searchTerm={searchTerm} onChangeSearch={setSearchTerm} />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemon={pokemonToDisplay} />
     </Container>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,17 @@
 import React from "react";
 
-function Search() {
+function Search({ searchTerm, onChangeSearch }) {
+
+  // set up a function to handle the change in our input
+  // this will pass the data back up to the parent component which sets the state
+  function handleChange(event) {
+    onChangeSearch(event.target.value)
+  }
+
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" />
+        <input className="prompt" value={searchTerm} onChange={handleChange} />
         <i className="search icon" />
       </div>
     </div>
